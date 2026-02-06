92a3aaa0bf9b4860125c67850c437995878890e6
PJYUCATAN\alejandro.gongora <alejandro.gongora@pjyucatan.gob.mx>
2026-02-06 14:52:02 -0600

    Carga inicial

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,28 @@
   const name="Vue dinámico"
   const stylecolor="color:blue"
   const arrayColores=["blue","red","peru"]
+  const arrayfrutas=[
+    {
+      name:"Manzana",
+      price:"$1.00",
+      description:"Una manzana",
+    },
+    {
+      name:"Pera",
+      price:"$2.00",
+      description:"Una pera",
+    },
+    {
+      name:"Naranja",
+      price:"$3.00",
+      description:"Una naranja",
+    },
+  ];
+  const objetoFruta={
+    name:"Manzana",
+    price:"$1.00",
+    id:1
+  };
   const activo=true
 </script>
 <template>
@@ -19,6 +41,21 @@
   <h4 v-else-if="activo === false">Estoy inactivo</h4>
   <p v-else>Estoy inactivo</p>
   <h2 v-show="activo">Estoy activo</h2>
+  <ul>
+    <li v-for="color in arrayColores">
+       {{ color }}
+    </li> 
+  </ul>
+  <ul>
+    <li v-for="fruta in arrayfrutas" :key="fruta.name">
+      {{ fruta.name }} - {{ fruta.description }} - {{ fruta.price }}
+    </li>
+  </ul>
+  <ul>
+    <li v-for="(value, propiedad, index) in objetoFruta":key="value">
+      {{ index }} - {{ propiedad }} : {{ value }}
+    </li>
+  </ul>
 </template>
 <style>
   h1{
